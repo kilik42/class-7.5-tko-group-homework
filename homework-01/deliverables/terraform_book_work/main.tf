29c388cf196635636e05e3311730cbdcb523d672
kilik42 <marvinevins@yahoo.com>
2026-04-30 17:37:59 -0500

    working on homework from terraform book

---
+++ homework-01/deliverables/terraform_book_work/main.tf
@@ -0,0 +1,14 @@
+resource "google_compute_instance" "this" {
+  project      = "training-416401"
+  name         = "cloudshell"
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
